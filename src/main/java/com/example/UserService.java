c90a49859a7e7ea1dca7268d555fa84f3c8f7242
Lakmina Pramodya Gamage <pramodyalakmina@gmail.com>
2026-01-30 08:38:23 +0530

    Introduce quality issues

--- src/main/java/com/example/UserService.java
+++ src/main/java/com/example/UserService.java
@@ -10,18 +10,11 @@ public class UserService {
     private String password = "admin123";
 
     // VULNERABILITY: SQL Injection
-    public void findUser(String username) throws Exception {
-
-        Connection conn =
-            DriverManager.getConnection("jdbc:mysql://localhost/db",
-                    "root", password);
-
+    public void deleteUser(String username) throws Exception {
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db", "root", password);
         Statement st = conn.createStatement();
-
-        String query =
-            "SELECT * FROM users WHERE name = '" + username + "'";
-
-        st.executeQuery(query);
+        String query = "DELETE FROM users WHERE name = '" + username + "'";
+        st.execute(query);
     }
 
     // SMELL: Unused method
