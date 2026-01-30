fbe8b10aaacbe0eb36a2ede03db0fbc42b93265a
Lakmina Pramodya Gamage <pramodyalakmina@gmail.com>
2026-01-30 09:09:01 +0530

    Refactor App and UserService classes for improved logging and query selection

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,7 +8,8 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         Calculator calc = new Calculator();
-        logger.info(String.valueOf(calc.calculate(10, 5, "add-again")));
+        int result = calc.calculate(10, 5, "add-again");
+        logger.info(String.valueOf(result));
         UserService service = new UserService();
         service.findUser("admin");
         service.deleteUser("admin"); // NEW dangerous call
